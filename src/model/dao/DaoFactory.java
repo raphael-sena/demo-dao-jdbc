5a342afe3235eb5efdc3e3bb9717819d4a3c5670
raphael sena <rsenares1@gmail.com>
2023-12-04 15:43:00 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,10 +1,11 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC(); //macete para deixar somente a interface
+		return new SellerDaoJDBC(DB.getConnection()); //macete para deixar somente a interface
 	}
 }
