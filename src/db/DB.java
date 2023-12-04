d19190e9813abcb48a29473a3a9dcfd08cb7ebdf
raphael sena <rsenares1@gmail.com>
2023-12-04 14:21:01 -0300

    Creating project and git repository

---
+++ src/db/DB.java
@@ -0,0 +1,70 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DB {
+	
+	private static Connection conn = null;
+	
+	//conecta com o banco de dados criados no mysql pegando as propriedades
+	public static Connection getConnection() {
+		if (conn == null) {
+			try {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return conn;
+	}
+	
+	//desfaz a conexao com o banco
+	public static void closeConnection() {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	private static Properties loadProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")){
+			Properties props = new Properties();
+			props.load(fs); //faz a leitura do arquivo properties apontado pelo fs
+			return props;
+		} catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+	
+	public static void closeStatement(Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+} 
